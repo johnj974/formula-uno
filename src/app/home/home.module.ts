2ad18756fd93b7be7437d4716a3af83ef4b59c2c
johnj974 <jjdug@hotmail.com>
2021-04-08 15:04:48 +0100

    home components added/ media links connected

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { MainHomeComponent } from './main-home/main-home.component';
+import { TeamComponent } from './team/team.component';
+import { TrackComponent } from './track/track.component';
+import { ResultComponent } from './result/result.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    MainHomeComponent
+    MainHomeComponent,
+    TeamComponent,
+    TrackComponent,
+    ResultComponent,
+    ShopComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule,
