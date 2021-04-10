a83accbf4b6d1c64126b46fbbbd8dbdffd4ed955
johnj974 <jjdug@hotmail.com>
2021-04-10 17:54:57 +0100

    media links added to contact section

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { MainContactComponent } from './main-contact/main-contact.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @NgModule({
   declarations: [
-    MainContactComponent
+    MainContactComponent,
+    ContactFormComponent
   ],
   imports: [
     CommonModule,
