b989a665027a5e638e5b1346cfbc8516e21d5561
johnj974 <jjdug@hotmail.com>
2021-04-13 20:04:09 +0100

    track model+service enhanced/track style updated

--- src/app/shared/models/track.model.ts
+++ src/app/shared/models/track.model.ts
@@ -1,11 +1,17 @@
 export class TrackModel {
   constructor(
     public name: string,
-    public country: string,
+    public type: string,
     public location: string,
     public distance: string,
     public laps: number,
     public latitude: number,
-    public longitude: number
+    public longitude: number,
+    public appearences: number,
+    public direction: string,
+    public turns: number,
+    public opened: string,
+    public capacity: number,
+    public grade: number
   ) {}
 }
--- src/app/shared/services/track.service.ts
+++ src/app/shared/services/track.service.ts
@@ -10,21 +10,33 @@ export class TrackService {
   trackServiceArray: TrackModel[] = [
     new TrackModel(
       'Albert Park',
-      'Australia',
-      'Melbourne',
-      '5.3km',
-      50,
+      'Street circuit',
+      'Melbourne, Austrailia',
+      '5.28km',
+      58,
       -37.849722,
-      144.968333
+      144.968333,
+      24,
+      'Clockwise',
+      14,
+      '20 November 1953',
+      800000,
+      1
     ),
     new TrackModel(
       'Autodroma Nazionale di Monza',
-      'Italy',
-      'Monza',
+      'Race Circuit',
+      'Monza, Italy',
       '5.793km',
-      50,
+      53,
       45.620556,
-      9.289444
+      9.289444,
+      70,
+      'Clockwise',
+      11,
+      '3 September 1922',
+      118865,
+      1
     ),
   ];
 
