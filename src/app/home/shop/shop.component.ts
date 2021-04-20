60049fdc2b9f73bbd70b99a5613eff5fc84fe904
johnj974 <jjdug@hotmail.com>
2021-04-20 12:39:57 +0100

    basic home page layout/buttons linked to pages

--- src/app/home/shop/shop.component.ts
+++ src/app/home/shop/shop.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toShop() {
+    this.router.navigate(['/shop']);
   }
-
 }
--- src/app/home/track/track.component.ts
+++ src/app/home/track/track.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
-  styleUrls: ['./track.component.scss']
+  styleUrls: ['./track.component.scss'],
 })
 export class TrackComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toTracks() {
+    this.router.navigate(['/tracks']);
   }
-
 }
