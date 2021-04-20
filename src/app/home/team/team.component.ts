394006d0c7a1915e993abc1b5a8bbaa4c4d4b6b0
johnj974 <jjdug@hotmail.com>
2021-04-20 14:38:42 +0100

    dynamic team display  home page+space on sections

--- src/app/home/team/team.component.ts
+++ src/app/home/team/team.component.ts
@@ -4,16 +4,28 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  styleUrls: ['./team.component.scss'],
 })
 export class TeamComponent implements OnInit {
+  //.
 
-  constructor(private router: Router) { }
+  teamName: string[] = ['ferrari', 'williams', 'alpha', 'aston', 'alpine'];
+  team: string;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.selectTeam();
   }
 
-  toTeams(){
-    this.router.navigate(["/teams"])
+  toTeams() {
+    this.router.navigate(['/teams']);
+  }
+
+  // display a differant team on load
+  selectTeam() {
+    let selectNumber = Math.floor(Math.random() * this.teamName.length);
+    console.log(selectNumber);
+    this.team = this.teamName[selectNumber];
   }
 }
