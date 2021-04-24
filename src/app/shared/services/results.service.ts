f3251b6dc302d1455e111b4163ddb4caf294f4d7
johnj974 <jjdug@hotmail.com>
2021-04-24 17:06:08 +0100

    remove tbl cols small view/improve team selection

--- src/app/shared/services/results.service.ts
+++ src/app/shared/services/results.service.ts
@@ -24,4 +24,12 @@ export class ResultsService {
       'http://ergast.com/api/f1/current/constructorStandings.json'
     );
   }
+
+  getRaceCalender() {
+    this.http.get(' http://ergast.com/api/f1/current.json');
+  }
 }
+//
+// this years race calender:  http://ergast.com/api/f1/current.json
+// http://ergast.com/api/f1/drivers/{{driverid}}
+// http://ergast.com/api/f1/drivers/alonso
